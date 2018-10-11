102b8c077f43c7c7ca4f326eabf5369eb4a3ac9a
AgusLespi <aguslespiaucq@gmail.com>
2018-10-11 17:10:02 +0200

    Script de arreglo de L-matrix en R

---
+++ R_scripts/Orquis_arrange.R
@@ -0,0 +1,61 @@
+###### ORQUIS
+###### ARREGLO DE DATOS PRIMARIOS
+
+###### Arreglo de L-matrix
+
+##### Importar datos de excel para sumar el número de presencias de cada sp. por estrato y forófito.
+
+# arreglo_L-spp_matrix.xls (hoja 3)
+dat <- read.excel() # from clipboard
+datos <- dat
+
+# variable de código combinado forofito-estrato
+datos$tstrat <- paste0(datos$ID.Forófito, sep="_", datos$Estrato_Orq)
+
+# pasar de integer a numérico
+datos[6:39] <- lapply(datos[6:39], as.numeric)
+
+# suma por código combinado
+sumas <- by(datos[,6:39], datos$tstrat, colSums)
+sumas1 <- data.frame(do.call("rbind", sumas))
+
+# reemplazar los valores > 1 por un 1 (matríz de presencias)
+dato <- sumas1
+dato1 <- as.data.frame(apply(dato, 2, function(x) ifelse(x>=1,1,0)))
+
+# recuperar el código de forófito y estrato
+code <- as.data.frame(rownames(dato1))
+	names(code) <- "code"
+	library(stringr)
+	code2 <- as.data.frame(str_split_fixed(code$code, "_", 2))
+	names(code2) <- c("tree","stratum") 
+
+# pegar código a tabla
+l.matrix0 <- cbind(code2, dato1)
+
+# exportar la tabla
+
+write.table(l.matrix0, "L-matrix.txt")
+
+################## LISTO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
