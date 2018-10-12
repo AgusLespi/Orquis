58492cb80e47fe5a9e1ba01a968e7665781941fc
AgusLespi <aguslespiaucq@gmail.com>
2018-10-12 14:50:16 +0200

    cree R-matrix en Orquis_arrange.R

--- R_scripts/Orquis_arrange.R
+++ R_scripts/Orquis_arrange.R
@@ -39,6 +39,39 @@ write.table(l.matrix0, "L-matrix.txt")
 
 ################## LISTO
 
+###### Arreglo de R-matrix
+
+##### Importar datos de excel para sumar el número de presencias de cada sp. por estrato y forófito.
+
+dat <- read.csv("par.csv", head=TRUE, sep=",") # 
+datos <- dat
+
+# variable de código combinado forofito-estrato
+datos$tstrat <- paste0(datos$Tree, sep="_", datos$Elev)
+
+# medias por código combinado
+sumas <- by(datos[,5:17], datos$tstrat, colMeans)
+sumas1 <- data.frame(do.call("rbind", sumas))
+dato1 <- sumas1
+
+# recuperar el código de forófito y estrato
+code <- as.data.frame(rownames(dato1))
+	names(code) <- "code"
+	library(stringr)
+	code2 <- as.data.frame(str_split_fixed(code$code, "_", 2))
+	names(code2) <- c("tree","stratum") 
+
+# pegar código a tabla
+r.matrix0 <- cbind(code2, dato1)
+
+# seleccionar las columnas utiles
+r.matrix1 <- r.matrix0[,c(1:2,4,7:9,13:15)] 
+
+# exportar la tabla
+
+write.table(r.matrix1, "R-matrix.txt")
+
+################## LISTO
 
 
 
